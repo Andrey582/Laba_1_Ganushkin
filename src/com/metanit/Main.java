6e38c8e2a3b9a9da3a6860a3e00b7b0e36a9e93c
Andrey582 <gandrey0606@mail.ru>
2022-01-19 21:38:40 +0300

    Initial commit

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,11 @@
+package com.metanit;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello Java!");
+
+        double a = 3, b = 4;
+        System.out.println(Math.sqrt(a * a + b * b));
+    }
+}
